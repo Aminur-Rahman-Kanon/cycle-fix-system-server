ad75c5395209103ff5d6b3a33f7e8c9938199027
Aminur-Rahman-Kanon <kanon754@gmail.com>
2023-02-01 18:13:48 +0000

    system commi1

---
+++ schema/schema.js
@@ -0,0 +1,56 @@
+const testimonialSchema = {
+    comment: {type: String, required: true},
+    name: {type: String, required: true},
+    rating: {type: 'Number', required: true}
+}
+
+const camsEnquirySchema = {
+    name : {type: String, required: true},
+    email : {type: String, required: true},
+    message: {type: String, required: true},
+    phoneNumber : {type: String, required: true}
+}
+
+const registrationSchema = {
+    firstName: String,
+    lastName: String,
+    email: String,
+    user: String,
+    password: String
+}
+
+const contactQuerySchema = {
+    name: String,
+    phone: String,
+    email: String,
+    message: String
+}
+
+const bookingSchema = {
+    service: String,
+    date: String,
+    packagePrice: String,
+    totalPrice: String,
+    paymentStatus: String,
+    authCode: String,
+    firstName: String,
+    lastName: String,
+    email: String,
+    phone: String,
+    bikeDetails: Object
+
+}
+
+const xiaomiQuery = {
+    service: String,
+    price: String
+}
+
+module.exports = {
+    testimonialSchema,
+    camsEnquirySchema,
+    registrationSchema,
+    contactQuerySchema,
+    bookingSchema,
+    xiaomiQuery
+}
---
+++ server.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const app = express();
+require('dotenv').config();
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+app.use(cors());
+
+//mongoDB initialization
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(res => console.log('database connected')).catch(err => console.log(err));
+
+//schema
+const testimonialSchema = require('./schema/schema').testimonialSchema;
+const camsEnquirySchema = require('./schema/schema').camsEnquirySchema;
+const registrationSchema = require('./schema/schema').registrationSchema;
+const contactQuerySchema = require('./schema/schema').contactQuerySchema;
+const bookingSchema = require('./schema/schema').bookingSchema;
+const xiaomiQuery = require('./schema/schema').xiaomiQuery;
+
+//model
+const testimonialModel = mongoose.model('testimonial', testimonialSchema);
+const camsEnquiryModel = mongoose.model('cams-enquiry', camsEnquirySchema);
+const registrationModel = mongoose.model('registered-user', registrationSchema);
+const contactQueryModel = mongoose.model('contact-query', contactQuerySchema);
+const bookingModel = mongoose.model('Customer-Booking', bookingSchema);
+const xiaomiModel = mongoose.model('xiaomi-query', xiaomiQuery);
+
+
+//route
+
+app.post('/bookings', async (req, res) => {
+    bookingModel.find({}).then(response => {
+        const filteredDates = response.sort((a, b) => new Date(a.date) - new Date(b.date));
+        const test1 = {};
+        let index = 0;
+        filteredDates.map(item => {
+            const date = item.date.split(' ').slice(1).join(' ');
+            if (test1.hasOwnProperty(date)){
+                index ++;
+                test1[date][index] = item;
+            }
+            else {
+                test1[date] = {};
+                index = 0;
+                test1[date][index] = item;
+            }
+        })
+        res.json({ status: 'success', data:test1 });
+    
+    })
+    .catch(err => res.json({ status: 'error' }));
+})
+
+
+
+
+
+app.listen('8000', () => {
+    console.log('server running on port 8000');
+})
